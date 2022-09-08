526a759595f5d1333e1f63254220df8064bf7cc5
Derrytobo <awulehany@gmail.com>
2022-09-08 14:54:52 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  * Return: Always 0 (sucess)
  */
 int main(void)
-}
-	puts("Programming is like building a multilingual puzzle"\n);
+{
+	puts("Programming is like building a multilingual puzzle"\in);
 	return (0);
 }
