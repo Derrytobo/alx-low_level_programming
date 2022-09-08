73b6099fa542771ba37842c6c378f201789e9c81
Derrytobo <awulehany@gmail.com>
2022-09-08 14:24:34 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+:
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	char Test[]="Programming is like building a multilingual puzzle";
+	puts(Test);
+	return 0;
+}
