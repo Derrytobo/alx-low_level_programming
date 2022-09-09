55ab1530a228962fdc91f41b73133ea63f3884f5
Derrytobo <awulehany@gmail.com>
2022-09-08 17:18:06 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	long long alonglongType;
 	float afloatType;
 
-	printf("size of a char: %d bytes\n", sizeof(charType) );
-	printf("size of an int: %d bytes\n", sizeof(intType) );
-	printf("size of a long int: %d bytes\n", sizeof(longintType) );
-	printf("size of a long long int: %d bytes\n", sizeof(alonglongType) );
-	printf("size of a float: %d bytes\n", sizeof(afloatType) );
+	printf("size of a char: %d bytes\n", sizeof(charType));
+	printf("size of an int: %d bytes\n", sizeof(intType));
+	printf("size of a long int: %d bytes\n", sizeof(longintType));
+	printf("size of a long long int: %d bytes\n", sizeof(alonglongType));
+	printf("size of a float: %d bytes\n", sizeof(afloatType));
 	return (0);
 }
