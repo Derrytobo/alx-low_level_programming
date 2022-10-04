71dd6c4859ff57e286604ad131385fc396adf773
Derrytobo <awulehany@gmail.com>
2022-10-04 05:55:34 -0700

    Task 3

---
+++ 0x0A-argc_argv/derry.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+
+{
+	printf("%d\n", argc);
+	int i;
+	for (i=0;i<argc;i++)
+	{
+		printf("argv index %d is = %s\n", i, argv[i]);
+	}
+	printf("argv index is %d is = %s", argc, argv[argc]);
+	return (0);
+}
