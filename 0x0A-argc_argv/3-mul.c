64e736b330c1817939d8c4634bc6ba08189ab482
Derrytobo <awulehany@gmail.com>
2022-10-04 06:02:57 -0700

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,23 +2,24 @@
 #include <stdlib.h>
 
 /**
- * main -> Multples of two number
- * @argc: argumenty count
- * @argv: array of arguments
- * result: 0
- */
+* main -> this is a function to print its name
+* @argc: argc parameter
+* @argv: an array of a command listed
+* Return: 0 for success
+*/
 
 int main(int argc, char *argv[])
 {
-	int result=0, num1,num2;
-	if (argc<3)
+	int result, num1, num2;
+
+	if (argc < 3)
 	{
-		printf("%s\n", Error);
+		printf("%s\n", "Error");
 		return (1);
 	}
-	num1=atoi(argv[1]);
-	num2=atoi(argv[2]);
-	result=(num1*num2);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = (num1 * num2);
 
 	printf("%d\n", result);
 	return (0);
